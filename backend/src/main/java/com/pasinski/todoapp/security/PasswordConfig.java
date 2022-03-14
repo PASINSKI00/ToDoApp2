24dd5ecdb6edb8ed431f6cc187a67a207be4d081
Krystian Pasinski <krystian.pasinski00@gmail.com>
2022-03-14 18:52:27 +0100

    added password config

--- backend/src/main/java/com/pasinski/todoapp/security/PasswordConfig.java
+++ backend/src/main/java/com/pasinski/todoapp/security/PasswordConfig.java
@@ -1,4 +1,13 @@
 package com.pasinski.todoapp.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 public class PasswordConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
 }
