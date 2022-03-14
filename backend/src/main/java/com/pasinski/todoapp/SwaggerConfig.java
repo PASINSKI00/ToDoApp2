b23457faee0197de71600d67e5d6fffb0839c2d3
Krystian Pasinski <krystian.pasinski00@gmail.com>
2022-03-14 20:14:44 +0100

    added swagger apiInfo

--- backend/src/main/java/com/pasinski/todoapp/SwaggerConfig.java
+++ backend/src/main/java/com/pasinski/todoapp/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.pasinski.todoapp;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,6 +14,12 @@ public class SwaggerConfig {
 
     @Bean
     public Docket docket(){
-        return new Docket(DocumentationType.SWAGGER_2).select().build();
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select().build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder().title("ToDoApp2 API")
+                .description("This is my second attempt at creating well documented, reusable API")
+                .version("v0.1").build();
     }
 }
