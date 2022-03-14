eb255734b14e924d4b374400ed9aedcf82d42146
Krystian Pasinski <krystian.pasinski00@gmail.com>
2022-03-14 19:17:36 +0100

    fixed small bugs

--- backend/src/main/java/com/pasinski/todoapp/security/ApplicationSecurityConfig.java
+++ backend/src/main/java/com/pasinski/todoapp/security/ApplicationSecurityConfig.java
@@ -1,8 +1,16 @@
 package com.pasinski.todoapp.security;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import static com.pasinski.todoapp.user.AppUserRole.USER;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -10,7 +18,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/","/css/*","/js/*","/api/*").permitAll()
-//                .antMatchers("/todo/**").hasRole(USER.name())
+                .antMatchers("/todo/**").hasRole(USER.name())
                 .anyRequest()
                 .authenticated()
                 .and()
