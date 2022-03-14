b89bbde7b67b4561fbf476c928c5643d0481d0b1
Krystian Pasinski <krystian.pasinski00@gmail.com>
2022-03-14 18:51:22 +0100

    added application Security Config

--- backend/src/main/java/com/pasinski/todoapp/security/ApplicationSecurityConfig.java
+++ backend/src/main/java/com/pasinski/todoapp/security/ApplicationSecurityConfig.java
@@ -1,4 +1,21 @@
 package com.pasinski.todoapp.security;
 
-public class ApplicationSecurityConfig {
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/","/css/*","/js/*","/api/*").permitAll()
+//                .antMatchers("/todo/**").hasRole(USER.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/todo",true);
+    }
 }
