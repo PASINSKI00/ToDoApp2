7045a2b0e46a97dbb163aade6ceccfca43e28ee1
Krystian Pasinski <krystian.pasinski00@gmail.com>
2022-03-19 12:21:15 +0100

    generated login-page and todo-page components

--- frontend/angular/src/app/app.module.ts
+++ frontend/angular/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { TodoPageComponent } from './todo-page/todo-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginPageComponent,
+    TodoPageComponent
   ],
   imports: [
     BrowserModule,
