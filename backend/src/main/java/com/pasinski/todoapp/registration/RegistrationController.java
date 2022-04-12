94c81109f8bb1bce03e1e09a09366d0149b2f5a7
Krystian Pasinski <krystian.pasinski00@gmail.com>
2022-04-12 15:00:07 +0200

    small changes to register API

--- backend/src/main/java/com/pasinski/todoapp/registration/RegistrationController.java
+++ backend/src/main/java/com/pasinski/todoapp/registration/RegistrationController.java
@@ -25,49 +25,19 @@ public class RegistrationController {
             @ApiResponse(code=201,
                     message = "CREATED",
                     response = AppUser.class,
-                    examples = @Example(value = {@ExampleProperty(value = "{\n" +
-                    "      \"id\": 1,\n" +
-                    "      \"name\": \"Krystian\",\n" +
-                    "      \"email\": \"email@email.com\",\n" +
-                    "      \"password\": \"$2a$10$3BiToe5D64G0uUJtTvY1geFgfAymVQAyS46rW/1rG5csIuc/qFeU6\",\n" +
-                    "      \"appUserRole\": \"USER\",\n" +
-                    "      \"locked\": false,\n" +
-                    "      \"enabled\": true,\n" +
-                    "      \"expired\": false,\n" +
-                    "      \"username\": \"email@email.com\",\n" +
-                    "      \"accountNonExpired\": true,\n" +
-                    "      \"credentialsNonExpired\": true,\n" +
-                    "      \"authorities\": [\n" +
-                    "        {\n" +
-                    "          \"authority\": \"task:read\"\n" +
-                    "        },\n" +
-                    "        {\n" +
-                    "          \"authority\": \"ROLE_USER\"\n" +
-                    "        },\n" +
-                    "        {\n" +
-                    "          \"authority\": \"task:write\"\n" +
-                    "        },\n" +
-                    "        {\n" +
-                    "          \"authority\": \"category:read\"\n" +
-                    "        },\n" +
-                    "        {\n" +
-                    "          \"authority\": \"category:write\"\n" +
-                    "        }\n" +
-                    "      ],\n" +
-                    "      \"accountNonLocked\": true\n" +
-                    "    }", mediaType = "application/json")})),
-            @ApiResponse(code = 400, message = "Passwords do not match", response = String.class, examples = @Example(value = {@ExampleProperty(value = "Passwords do not match", mediaType = "application/json")})),
-            @ApiResponse(code = 401, message = "Email already taken", response = String.class, examples = @Example(value = {@ExampleProperty(value = "Email already taken", mediaType = "application/json")}))
+                    examples = @Example(value = {@ExampleProperty(value = "User created successfully", mediaType = "application/json")})),
+            @ApiResponse(code = 400, message = "BAD REQUEST", response = String.class, examples = @Example(value = {@ExampleProperty(value = "Passwords do not match", mediaType = "application/json")})),
+            @ApiResponse(code = 401, message = "UNAUTHORIZED", response = String.class, examples = @Example(value = {@ExampleProperty(value = "Email already taken", mediaType = "application/json")}))
     })
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping()
-    public ResponseEntity<?> signUpUser(@Valid @RequestBody RegistrationForm registrationForm){
+    public ResponseEntity<String> signUpUser(@Valid @RequestBody RegistrationForm registrationForm){
         AppUser appUser;
         try {
             appUser = registrationService.signUpUser(registrationForm);
         }catch (Exception e){
             return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<AppUser>(appUser,HttpStatus.CREATED);
+        return new ResponseEntity<String>("User created successfully",HttpStatus.CREATED);
     }
 }
